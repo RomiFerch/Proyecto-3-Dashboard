f55924059d389ba034e21b99669bf15e8f0b6352
Fgutierreza01 <fgutierreza01@gmail.com>
2022-08-28 18:53:14 -0400

    cambio de api y obtener nuevis dato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 let url='https://api.themoviedb.org/3/movie/popular?api_key=617fd88a44663150d05a15b5b030d50e&language=es-LA'
 async function obtenerDatos(){
-    let respuesta= await axios.get(url)
+    let respuesta= await axios.get(url,JSON);
+    let datos= await respuesta.data
     console.log(respuesta.data)
+    datos.results.forEach(pelicula => {
+        console.log(pelicula.title)
+        console.log(pelicula.vote_average)
+    });
 }
 obtenerDatos()
